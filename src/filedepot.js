8473d5beb7a6f180173720c24f89ca0b0a278f6d
Sam-Mauris Yong <mauris@hotmail.sg>
2017-08-21 14:34:53 +0800

    add eslint rc

--- src/filedepot.js
+++ src/filedepot.js
@@ -9,7 +9,7 @@ program
   .version(package.version)
   .command('create [path]', 'Create a new bucket where files are stored at [path]')
   .command('remove [bucketId]', 'Remove a bucket and delete all files in the bucket')
-  .command('list', 'list all buckets', {isDefault: true})
+  .command('list', 'list all buckets', { isDefault: true })
   .command('key [bucketId] [origin]', 'Creates an access key for a bucket from an origin')
   .command('revoke [keyId]', 'Revokes the access of a given key')
   .command('keys [bucketId]', 'List of access keys of a bucket')
